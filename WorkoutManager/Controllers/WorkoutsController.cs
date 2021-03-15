eab92c5db632c651e9b48ee07f63fbce0b3ca520
Lesya <olesyaoleksyuk@mail.ru>
2021-03-15 16:34:18 +0300

    Add a New Controller

--- WorkoutManager/Controllers/WorkoutsController.cs
+++ WorkoutManager/Controllers/WorkoutsController.cs
@@ -2,147 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WorkoutManager.Data;
 using WorkoutManager.Models;
 
 namespace WorkoutManager.Controllers
 {
-    public class WorkoutsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutsController : ControllerBase
     {
-        private readonly WorkoutContext _context;
+        private readonly WorkoutsContext _context;
 
-        public WorkoutsController(WorkoutContext context)
+        public WorkoutsController(WorkoutsContext context)
         {
             _context = context;
         }
 
-        // GET: Workouts
-        public async Task<IActionResult> Index()
+        // GET: api/Workouts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Workout>>> GetWorkout()
         {
-            return View(await _context.Workout.ToListAsync());
+            return await _context.Workout.ToListAsync();
         }
 
-        // GET: Workouts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: api/Workouts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Workout>> GetWorkout(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var workout = await _context.Workout
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (workout == null)
-            {
-                return NotFound();
-            }
-
-            return View(workout);
-        }
-
-        // GET: Workouts/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Workouts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date,DistanceInMeters,TimeInSeconds")] Workout workout)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(workout);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(workout);
-        }
-
-        // GET: Workouts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var workout = await _context.Workout.FindAsync(id);
+
             if (workout == null)
             {
                 return NotFound();
             }
-            return View(workout);
+
+            return workout;
         }
 
-        // POST: Workouts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,DistanceInMeters,TimeInSeconds")] Workout workout)
+        // PUT: api/Workouts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWorkout(int id, Workout workout)
         {
             if (id != workout.Id)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            if (ModelState.IsValid)
+            _context.Entry(workout).State = EntityState.Modified;
+
+            try
             {
-                try
-                {
-                    _context.Update(workout);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!WorkoutExists(workout.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
             }
-            return View(workout);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkoutExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: Workouts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // POST: api/Workouts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Workout>> PostWorkout(Workout workout)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            _context.Workout.Add(workout);
+            await _context.SaveChangesAsync();
 
-            var workout = await _context.Workout
-                .FirstOrDefaultAsync(m => m.Id == id);
+            return CreatedAtAction("GetWorkout", new { id = workout.Id }, workout);
+        }
+
+        // DELETE: api/Workouts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWorkout(int id)
+        {
+            var workout = await _context.Workout.FindAsync(id);
             if (workout == null)
             {
                 return NotFound();
             }
 
-            return View(workout);
-        }
-
-        // POST: Workouts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var workout = await _context.Workout.FindAsync(id);
             _context.Workout.Remove(workout);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
         private bool WorkoutExists(int id)
--- WorkoutManager/Startup.cs
+++ WorkoutManager/Startup.cs
@@ -37,6 +37,9 @@ namespace WorkoutManager
 
             services.AddDbContext<WorkoutContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("WorkoutContext")));
+
+            services.AddDbContext<WorkoutsContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WorkoutsContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
