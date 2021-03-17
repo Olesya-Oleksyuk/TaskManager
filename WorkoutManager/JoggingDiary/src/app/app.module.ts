e0530a18dfab5e1d490b92901afdda0abb12683d
Lesya <olesyaoleksyuk@mail.ru>
2021-03-17 19:57:20 +0300

    Set up Angular initials & create required components

---
+++ WorkoutManager/JoggingDiary/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GridJoggingComponent } from './grid-jogging/grid-jogging.component';
+import { AddOrUpdateJoggingComponent } from './add-or-update-jogging/add-or-update-jogging.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GridJoggingComponent,
+    AddOrUpdateJoggingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WorkoutManager/JoggingDiary/src/app/grid-jogging/grid-jogging.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-jogging',
+  templateUrl: './grid-jogging.component.html',
+  styleUrls: ['./grid-jogging.component.css']
+})
+export class GridJoggingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
